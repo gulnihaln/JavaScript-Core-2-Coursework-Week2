3041720110eab9b216d7abb1c23c524d89c4f6b7
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-07-28 16:46:06 +0100

    A is done

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,15 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  arrayOfPeople.forEach(person => {
+    let namesH1 = document.createElement('h1');
+    let jobsH2 = document.createElement('h2');
+
+    namesH1.innerHTML = person.name;
+    jobsH2.innerHTML = person.job;
+
+    content.appendChild(namesH1);
+    content.appendChild(jobsH2);
+  });
 }
 
 let people = [
