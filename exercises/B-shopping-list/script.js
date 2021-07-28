ba5f5312019b67c425db3086324e30bad9daf933
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-07-28 17:06:27 +0100

    B is done

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,16 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(shoppingItems) {
+  let content = document.getElementById('content');
+  let list = document.createElement('ul');
+
+  //li
+  shoppingItems.forEach( el => {
+    let itemsListed = document.createElement('li');
+    itemsListed.innerHTML = el;
+
+    content.appendChild(list);
+    list.appendChild(itemsListed);
+  });
+  
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
