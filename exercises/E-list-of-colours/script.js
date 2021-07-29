dda001fdbe12c350dbcf9b0142c38a53bc390d29
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-07-29 12:26:18 +0100

    E is done

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,21 @@
 function listOfColours(colours) {
-  // Write your code here...
+    const content = document.getElementById('content');
+    const selectEl = document.createElement('select');
+    const pEl = document.createElement('p');
+    content.appendChild(selectEl);
+    content.appendChild(pEl);
+
+    colours.forEach(colour => {
+      const optionEl = document.createElement('option');
+      selectEl.appendChild(optionEl);
+      optionEl.innerText = colour; 
+    });
+    
+    selectEl.addEventListener('change', () => {
+      pEl.style.color = selectEl.value;
+      pEl.innerText = `You have selected: ${selectEl.value}`
+    });
+    
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
