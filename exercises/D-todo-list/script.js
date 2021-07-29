04aeedd81004bb836e67df5ebc529430026e7ca6
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-07-29 12:01:50 +0100

    D is done

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,21 @@
 function todoList(todos) {
-  // Write your code here...
+  const content = document.getElementById('content');
+  const ulEl = document.createElement('ul');
+  content.appendChild(ulEl);
+
+  todos.forEach( element =>{
+    const liEl = document.createElement('li');
+    liEl.innerText = element.todo;
+    ulEl.appendChild(liEl);
+
+    liEl.addEventListener('click', ()=>{
+      if (liEl.style.textDecorationLine === 'line-through'){
+        liEl.style.textDecorationLine = 'none';
+      } else {
+        liEl.style.textDecorationLine = 'line-through';
+      }
+    });
+  });
 }
 
 const todos = [
