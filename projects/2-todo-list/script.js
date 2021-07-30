9a66edf31ead4d6ed7628774046e82ee62875412
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-07-30 21:51:26 +0100

    2-todo-list done

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,22 +1,40 @@
 function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
-  
-  todos.forEach(element => {
+  const list = document.getElementById("todo-list");
+  list.innerHTML = ''; // yeniden yazilan array leri kaldirmak icin
+
+  todos.forEach((element, index) => {
+    
     const liEl = document.createElement('li');
     list.appendChild(liEl);
-    liEl.classList.add('list-group-item d-flex justify-content-between align-items-center');
-    
+    liEl.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    liEl.innerText = element.task;
+    if(element.completed){
+      liEl.style.textDecorationLine = 'line-through';
+    }
     const spanEl = document.createElement('span');
     liEl.appendChild(spanEl);
-    spanEl.classList.add('badge bg-primary rounded-pill');
+    spanEl.classList.add('badge', 'bg-primary', 'rounded-pill');
 
     const iCheck = document.createElement('i');
     spanEl.appendChild(iCheck);
-    iCheck.classList.add('fa fa-check');
+    iCheck.classList.add('fa', 'fa-check');
+    iCheck.addEventListener('click', ()=> {
+      if(element.completed){
+        element.completed = false;
+        liEl.style.textDecorationLine = 'none';
+      }else {
+        element.completed = true;
+        liEl.style.textDecorationLine = 'line-through';
+      }
+    });
 
     const iTrash = document.createElement('i');
     spanEl.appendChild(iTrash);
-    iTrash.classList.add('fa fa-trash');
+    iTrash.classList.add('fa', 'fa-trash');
+    iTrash.addEventListener('click', ()=>{
+    todos.splice(index, 1);
+    populateTodoList(todos); //recursive 
+    });
 
     
   });
@@ -30,7 +48,7 @@ function populateTodoList(todos) {
 // after you have created them using JavaScript
 let todos = [
   { task: "Wash the dishes", completed: false },
-  { task: "Do the shopping", completed: false },
+  { task: "Do the shopping", completed: true },
 ];
 
 populateTodoList(todos);
@@ -39,14 +57,22 @@ populateTodoList(todos);
 // add it as a new todo to the bottom of the todo list. 
 // These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
+    const todoInputEl = document.getElementById('todoInput');
+    todos.push({task: todoInputEl.value, completed: false});
+    populateTodoList(todos);
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
 }
 
-// Advanced challenge: Write a fucntion that checks the todos in the todo list and 
+// Advanced challenge: Write a function that checks the todos in the todo list and 
 // deletes the completed ones (we can check which ones are completed by 
 // seeing if they have the line-through styling applied or not).
-function deleteAllCompletedTodos() {
+function deleteAllCompletedTodos(event) {
+    todos = todos.filter(element =>{
+      return !element.completed;
+    })
+    event.preventDefault();
+    populateTodoList(todos);
   // Write your code here...
 }
